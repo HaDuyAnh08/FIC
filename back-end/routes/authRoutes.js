441e02999142cfb7d4d38d284f23f748e21c3274
dungdv <21021273@vnu.edu.vn.com>
2025-06-22 14:11:41 +0700

    chia back_end (controller book, login)

---
+++ back-end/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { googleAuth, googleCallback } = require('../controllers/login/authController');
+
+router.get('/auth/google', googleAuth);
+router.get('/auth/google/callback', googleCallback);
+
+module.exports = router;
--- backend/loadRoutes.js
+++
@@ -1,5 +0,0 @@
-const apiRouter = require('./routes/bookRoutes');
-
-module.exports = function (app) {
-  app.use('/api/books', apiRouter);
-};
