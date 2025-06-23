49b1ff8399c8c506f71dedbee44ac11994afbf4a
HaDuyAnh08 <duyanhcn6k66@gmail.com>
2025-06-23 09:12:00 +0700

    cleaned_book.json

--- back-end/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { googleAuth, googleCallback } = require("../controllers/login/authController");
-
-router.get("/auth/google", googleAuth);
-router.get("/auth/google/callback", googleCallback);
-
-module.exports = router;
